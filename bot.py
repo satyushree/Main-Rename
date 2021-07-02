e45429a622b649423ce8ca6d31bfd145a6045d99
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-03 01:21:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(
-        root="plugins"
+        root="renamer/plugins"
     )
     app = pyrogram.Client(
         "Renamer",
