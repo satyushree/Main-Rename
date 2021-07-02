93f8fea2766959c67a6faf507103e344349e5d1f
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-03 00:12:42 +0530

    Update __main__.py

--- renamer/__main__.py
+++ renamer/__main__.py
@@ -21,24 +21,21 @@ if bool(os.environ.get("WEBHOOK", False)):
 else:
     from renamer.config import Config
 
-import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
 
 if __name__ == "__main__" :
     # create download directory, if not exist
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(
-        root="plugins"
+        root="renamer/plugins"
     )
     app = pyrogram.Client(
         "[ARM] RENAME",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
-        plugins=dict(root="renamer/plugins"),
-                 workers=100)
+        plugins=plugins
+    )
     
     app.start()
 
