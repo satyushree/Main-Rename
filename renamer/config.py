d530193dd9f394a6cf6612832e400ab2c50fd77b
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-03 01:18:12 +0530

    Update config.py

--- renamer/config.py
+++ renamer/config.py
@@ -4,6 +4,8 @@ logger = logging.getLogger(__name__)
 
 
 class Config:
+    # get a token from https://chatbase.com
+    CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     OWNER_ID =  int(os.environ.get("OWNER_ID", ""))
