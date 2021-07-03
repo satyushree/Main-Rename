fd436987553e960cbbfa7ef481f85ea8c235c8b4
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-04 00:45:27 +0530

    Update database1.py

--- renamer/database1/database1.py
+++ renamer/database1/database1.py
@@ -6,6 +6,7 @@ import motor.motor_asyncio
 class Database:
     
     def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
     
