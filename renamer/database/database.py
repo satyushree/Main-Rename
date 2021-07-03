11ffef83fe573d17bb39ae2359a789046bd73811
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-03 23:30:21 +0530

    Update database.py

--- renamer/database/database.py
+++ renamer/database/database.py
@@ -2,10 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-import datetime
-
-import motor.motor_asyncio
-
 import os
 
 import threading
@@ -36,92 +32,12 @@ class Database(BASE):
     upload_mode = Column(Boolean)
     is_logged = Column(Boolean)
 
-    def __init__(self, id, thumb_id, upload_mode, is_logged, uri, database_name):
+    def __init__(self, id, thumb_id, upload_mode, is_logged):
         self.id = id
         self.thumb_id = thumb_id
         self.upload_mode = upload_mode
         self.is_logged = is_logged
-        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.col = self.db.users0
-        
-    def new_user(self, id):
-        return dict(
-            id = id,
-            join_date = datetime.date.today().isoformat(),
-            ban_status=dict(
-                is_banned=False,
-                ban_duration=0,
-                banned_on=datetime.date.max.isoformat(),
-                ban_reason=''
-            )
-        )
-    async def add_user(self, id):
-        user = self.new_user(id)
-        await self.col.insert_one(user)
-    
-    
-    async def is_user_exist(self, id):
-        user = await self.col.find_one({'id':int(id)})
-        return True if user else False
-    
-    
-    async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
 
-    async def remove_ban(self, id):
-        ban_status = dict(
-            is_banned=False,
-            ban_duration=0,
-            banned_on=datetime.date.max.isoformat(),
-            ban_reason=''
-        )
-        await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
-    
-    
-    async def ban_user(self, user_id, ban_duration, ban_reason):
-        ban_status = dict(
-            is_banned=True,
-            ban_duration=ban_duration,
-            banned_on=datetime.date.today().isoformat(),
-            ban_reason=ban_reason
-        )
-        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': ban_status}})
-    async def get_ban_status(self, id):
-        default = dict(
-            is_banned=False,
-            ban_duration=0,
-            banned_on=datetime.date.max.isoformat(),
-            ban_reason=''
-        )
-        user = await self.col.find_one({'id':int(id)})
-        return user.get('ban_status', default)
-    
-    
-    async def get_all_banned_users(self):
-        banned_users = self.col.find({'ban_status.is_banned': True})
-        return banned_users
-
-
-    async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
-    
-    
-    async def delete_user(self, user_id):
-        await self.col.delete_many({'id': int(user_id)})
-        
-    async def get_last_used_on(self, id):
-        user = await self.get_user(id)
-        return user.get("last_used_on", datetime.date.today().isoformat())
-    
-    async def get_user(self, id):
-        user = self.cache.get(id)
-        if user is not None:
-            return user
-        
-    
 Database.__table__.create(checkfirst=True)
 
 async def update_login(id, is_logged):
@@ -166,6 +82,7 @@ async def del_user(id):
             return True
         else:
             return False
+
 async def get_data(id):
     try:
         user_data = SESSION.query(Database).get(id)
